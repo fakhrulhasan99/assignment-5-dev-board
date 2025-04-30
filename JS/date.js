ec3300d38bac405824beae96c3d7f404f13a4691
fakhrulhasan99 <fakhrulhasan.faf1@gmail.com>
2025-04-30 10:52:46 +0600

    first commit

---
+++ JS/date.js
@@ -0,0 +1,13 @@
+// today's date
+const date = document.getElementById("today-date");
+const today = new Date();
+const dayName = today.toLocaleDateString(undefined, { weekday: 'long'});
+const dateName = today.toLocaleDateString(undefined, {year : 'numeric', month : 'long', day : 'numeric'});
+date.innerHTML = `${dayName}<br>${dateName}`;
+
+// deadline 5 days later
+const deadline = document.getElementById("deadline");
+const deadLine = new Date(today);
+deadLine.setDate(today.getDate()+5);
+const deadLineName = deadLine.toLocaleDateString(undefined, {year : 'numeric', month : 'long', day : 'numeric'});
+deadline.innerHTML = `${deadLineName}`;
